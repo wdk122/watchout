aed7a3b708fe11f3074c0b033ac630d152915255
Bill Kilmer <wdk122@gmail.com>
2015-04-03 15:26:25 -0700

    Draws enemy circles on svg game board.

--- watchout.js
+++ watchout.js
@@ -1,2 +1,22 @@
 // start slingin' some d3 here.
+var width = 700;
+var height = 450;
+var enemyCount = 30;
+var radius = 10;
+var x, y;
 
+var data = [];
+// [o]
+for(var i = 0; i < enemyCount; i++){
+  x = radius + Math.floor(Math.random() * (width -  2 * radius));
+  y = radius + Math.floor(Math.random() * (height - 2 * radius));
+  data.push({x: x, y: y});
+}
+console.log(data);
+
+
+var board = d3.select('#board');
+
+board.selectAll('circle').data(data).enter().append('circle').attr('cx', function(d) {return d.x;})
+                                                            .attr('cy', function(d) {return d.y;})
+                                                            .attr('r', radius);
