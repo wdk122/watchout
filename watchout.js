d11f5cee534b7ce59952bb6ec868350b74b060ed
Bill Kilmer <wdk122@gmail.com>
2015-04-03 15:56:12 -0700

    Makes it so that the enemies move to a new random location every second.

--- watchout.js
+++ watchout.js
@@ -4,19 +4,35 @@ var height = 450;
 var enemyCount = 30;
 var radius = 10;
 var x, y;
+var randX = function(){
+  return radius + Math.floor(Math.random() * (width -  2 * radius));
+};
+var randY = function(){
+  return radius + Math.floor(Math.random() * (height -  2 * radius));
+};
+
 
 var data = [];
 // [o]
 for(var i = 0; i < enemyCount; i++){
-  x = radius + Math.floor(Math.random() * (width -  2 * radius));
-  y = radius + Math.floor(Math.random() * (height - 2 * radius));
+  x = randX();
+  y = randY();
   data.push({x: x, y: y});
 }
-console.log(data);
-
 
 var board = d3.select('#board');
 
-board.selectAll('circle').data(data).enter().append('circle').attr('cx', function(d) {return d.x;})
-                                                            .attr('cy', function(d) {return d.y;})
-                                                            .attr('r', radius);
+var enemies = board.selectAll('circle')
+  .data(data).enter()
+  .append('circle');
+
+enemies.attr('cx', function(d) {return d.x;})
+  .attr('cy', function(d) {return d.y;})
+  .attr('r', radius);
+
+
+setInterval(function(){
+  enemies
+    .attr('cx', randX)
+    .attr('cy', randY);
+  } , 1000);
